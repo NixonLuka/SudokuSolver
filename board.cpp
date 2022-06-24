72c9392f8665e723a03edf30773522fe17ba344f
Luka Nixon <lenixon@ucdavis.edu>
2022-06-24 16:53:57 -0700

    Board set up

---
+++ board.cpp
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <algorithm>
+#include <sstream>
+#include "board.hpp"
+
+Tile::Tile(int val){
+    this->value = val;
+    if (val != 0) {
+        this->concrete = true;
+    } else {
+        this->concrete = false;
+    }
+}
+
+
+bool Tile::isConcrete(){
+    return concrete;
+}
+
+int Tile::getValue(){
+    return value;
+}
+bool Tile::setValue(int val){
+    auto iter = std::find(begin(available), end(available), val);
+    if (iter != available.end()){ //is available
+        available.erase(iter,iter+1);
+        value = val;
+        return true;
+    } 
+    return false;
+}
+
+bool Tile::operator==(Tile& tile){
+    if (this->getValue() == tile.getValue()){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//iterate over incoming and add it to rows and columns, validate that values are betwenn 0-9
+Board::Board(std::vector<int> incoming){
+    auto iter = incoming.begin();
+    //tiles.resize(9);
+    for(int i = 0; i < 9; i++){
+        std::vector<Tile> row;
+        //row.resize(9);
+        for(int j = 0; j < 9; j++){
+            if (*iter < 0 || *iter > 9){
+                std::cout << "Value in intial board is out of bounds." << std::endl;
+                return;
+            }
+            Tile tile(*iter);
+            row.push_back(tile);
+            ++iter;
+        }
+        tiles.push_back(row);
+    }
+}
+
+//check for non zero duplicates in vector
+bool Board::checkRow(int row, int column){
+    Tile placedTile = tiles[row][column];
+    for (int i = 0; i < 9; i++){
+        if(i == column)
+            continue;
+        if (placedTile == tiles[row][i])
+            return false;
+    }
+    return true;
+}
+
+bool Board::checkColumn(int row, int column){
+    Tile placedTile = tiles[row][column];
+    for(int i = 0; i < 9; i++){
+        if (i == row)
+            continue;
+        if (placedTile == tiles[i][column])
+            return false;
+    }
+    return true;
+}
+
+//always going to be called after checking row and column, so we will not repeat comparisons (hence the continues in the nested loop)
+bool Board::checkSquare(int row, int column){
+    Tile placedTile = tiles[row][column];
+    int lowerRowBound = row - (row % 3);
+    int lowerColumnBound = column - (column % 3);
+    for(int i = lowerRowBound; i < lowerRowBound + 2; i++){
+        if (i == row)
+            continue;
+        for (int j = lowerColumnBound; j < lowerColumnBound + 2; j++){
+            if (j == column)
+                continue;
+            if(placedTile == tiles[i][j])
+                return false;
+        }
+    }
+    return true;
+}
+
+//tile is placed, verify that it works
+bool Board::validTile(int row, int column){
+    if (checkRow(row,column) && checkColumn(row,column) && checkSquare(row,column))
+        return true;
+    else
+        return false;
+}
+
+void Board::printBoard(){
+    std::string horizontal(25, '-');
+    std::cout << std::endl << horizontal << std::endl;
+    for(int i = 0; i < 9; i++){
+        std::cout << "| ";
+        for (int j = 0; j < 9; j++){
+            if((j+1) % 3== 0)
+                std::cout << tiles[i][j].getValue() << " | ";
+            else
+                std::cout << tiles[i][j].getValue() << " ";
+        }
+        if((i+1) % 3 == 0)
+            std::cout << std::endl << horizontal << std::endl;
+        else
+             std::cout << std::endl;
+    }
+}
+
+int main(int argc, char* argv[]){
+    int input;
+    std::vector<int> board;
+    //for user inputed board
+    /*while (board.size() < 81 && std::cin >> input){
+        board.push_back(input);   
+    }*/
+    std::stringstream in;
+    in <<  "1 2 3 4 5 6 7 8 9\
+            1 2 3 4 5 6 7 8 9\
+            1 2 3 4 5 6 7 8 9\
+            1 2 3 4 5 6 7 8 9\
+            1 2 3 4 5 6 7 8 9\
+            1 2 3 4 5 6 7 8 9\
+            1 2 3 4 5 6 7 8 9\
+            1 2 3 4 5 6 7 8 9\
+            1 2 3 4 5 6 7 8 9";
+    while(in >> input){
+        board.push_back(input);
+    }
+    
+    Board testBoard(board);
+    testBoard.printBoard();
+}
---
+++ board.hpp
@@ -0,0 +1,29 @@
+#include <vector> 
+
+class Tile{
+    private:
+        int value; //1-9, 0 for empty
+        bool concrete;
+        std::vector<int> available{1,2,3,4,5,6,7,8,9};
+    public:
+        Tile(){
+            value = 0;
+        }
+        Tile(int);
+        bool operator==(Tile&);
+        bool isConcrete();
+        int getValue();
+        bool setValue(int);
+};
+
+class Board{
+    private:
+        std::vector<std::vector<Tile>> tiles;
+        bool checkRow(int,int);
+        bool checkColumn(int,int);
+        bool checkSquare(int,int);
+    public:
+        Board(std::vector<int>);    
+        bool validTile(int, int);
+        void printBoard();
+};
